6f68cc453c8162267609fbf1557adb9fb5737153
SzedlacsekGigi <szedlacsek.lilla@gmail.com>
2020-02-03 17:34:27 +0100

    minden

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Link} from "react-router-dom";
 import Navigation from './components/Navigation';
 import './App.css';
 import './stylesheet.css';
@@ -7,7 +8,9 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <Navigation/>
+                <Router>
+                    <Link to="/"><Navigation/></Link>
+                </Router>
             </div>
         )
     }
