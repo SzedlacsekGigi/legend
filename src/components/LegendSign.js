f6e20b0b5aedd95d143a85616a781838b8dd8be4
SzedlacsekGigi <szedlacsek.lilla@gmail.com>
2020-02-03 17:22:01 +0100

    everything else

--- src/components/LegendSign.js
+++ src/components/LegendSign.js
@@ -5,9 +5,9 @@ class LegendSign extends Component{
 
     render() {
         return(
-            <h1 className="legend-sign">
+            <h3 className="legend-sign">
                 Heroes come and go, but LEGENDS are forever
-            </h1>
+            </h3>
         )
     }
 }
